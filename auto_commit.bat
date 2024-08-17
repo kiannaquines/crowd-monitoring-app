246d9fe4a8719c9fb6ed2902ea06488dc597f367
Kian Naquines <kjgnaquines@gmail.com>
2024-08-17 23:00:14 +0800

    Update auto_commit.bat

--- auto_commit.bat
+++ auto_commit.bat
@@ -7,7 +7,7 @@ git add .
 git commit -m "%commit_message%"
 git push -u origin %branch_name%
 
-echo You have successfully commit and push the project changes...
+echo You have successfully commit and push the project changes......
 
 timeout /t 6 /nobreak
 
